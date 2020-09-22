c01479461ac3c742d91d99b97ac76bc2b8b0eb60
Tom Hinton <tomjhinton@gmail.com>
2020-09-22 11:47:29 +0100

    basic principle

--- src/app.js
+++ src/app.js
@@ -1,1 +1,36 @@
-const rapidLib = window.RapidLib();
+const rapidLib = RapidLib()
+
+
+
+var myRegression = new rapidLib.Regression()
+
+const myClassification = new rapidLib.Classification()
+
+const mySeriesClassification  = new rapidLib.SeriesClassification()
+
+var myData = [
+          {
+            input:  [48],
+            output: [130.81]
+          },
+          {
+            input:  [54],
+            output: [185.00]
+          },
+          {
+            input:  [60],
+            output: [261.63]
+          },
+          {
+            input:  [66],
+            output: [369.994]
+          },
+          {
+            input:  [72],
+            output: [523.25]
+          }
+        ];
+
+myRegression.train(myData)
+
+console.log(myRegression.run([64]))
